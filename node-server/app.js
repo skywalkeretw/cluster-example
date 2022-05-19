225c82c1aa73c1a62cddfd4c52b3ef18e5e9a5f3
Luke Roy <lukeroy@Lukes-MacBook-Pro.local>
2022-05-19 10:36:25 +0200

    add node example

---
+++ node-server/app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+
+const app = express()
+const port = 8080
+app.use(express.urlencoded({extended: true}));
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).send(JSON.stringify({"body": "Hello Express"}))
+})
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
